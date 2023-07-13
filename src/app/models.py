b1a90c59053fb1b3460dd3f7d4190a79ecd51c75
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-13 09:03:29 -0300

    created app models

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    enrolled_in = models.ManyToManyField('Course', blank=True)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student, blank=True)
