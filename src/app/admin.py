7695ac5f8391997d7f22b065b1ae4ac21e4e67a5
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-13 09:32:03 -0300

    register models in admin

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import Topic, Student, Course
 # Register your models here.
+
+admin.site.register(Student)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'students_count')
+
+    def students_count(self, obj):
+        return obj.student_set.count()
+
+    students_count.short_description = 'Cantidad de estudiantes'
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'courses_count')
+
+    def courses_count(self, obj):
+        return obj.course_set.count()
+
+    courses_count.short_description = 'Cantidad de cursos'
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Topic, TopicAdmin)
--- src/app/models.py
+++ src/app/models.py
@@ -7,15 +7,25 @@ class Topic(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Student(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     enrolled_in = models.ManyToManyField('Course', blank=True)
 
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Topic, on_delete=models.CASCADE, verbose_name='Categoría')
     students = models.ManyToManyField(Student, blank=True)
+
+    def __str__(self):
+        return self.name
