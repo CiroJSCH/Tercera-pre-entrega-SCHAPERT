04e6cafb94a5c4bae9e5be2caed8e907feb135c7
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-13 18:12:53 -0300

    main view

--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .forms import TopicForm, StudentForm, CourseForm
+from .models import Topic, Student, Course
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main.html')
+    topic_form = TopicForm()
+    student_form = StudentForm()
+    course_form = CourseForm()
+
+    topics = Topic.objects.all()
+    courses = Course.objects.all()
+    students = Student.objects.all()
+
+    return render(request, 'main.html', {'topic_form': topic_form, 'student_form': student_form, 'course_form': course_form, 'topics': topics, 'courses': courses, 'students': students})
